6905b9b6e5542c3fe0cab6ae0b77b04c0e897f5b
Anaïs Wucher <anais.wucher@hetic.net>
2015-02-11 12:23:06 +0100

    Ajout des affiches et des liens vers les films

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,18 +2,18 @@ class MoviesController < ApplicationController
 	
 	def home
 
-		@new_trailers = Array.new
-		movies = Tmdb::Movie.now_playing
+		#@new_trailers = Array.new
+		@movies = Tmdb::Movie.now_playing
 
-		movies.each do |movie|
-			@img = Tmdb::Movie.detail(movie.id)
-			@new_trailers.push(@img)
-		end 
+		#movies.each do |movie|
+			#@img = Tmdb::Movie.detail(movie.name)
+			#@new_trailers.push(@img)
+		#end 
 	
 	end
 
 	def get_trailers
-		return Tmdb::Movie.trailers(movieid)
+		#return Tmdb::Movie.trailers(movieid)
 	end
 	
 end
